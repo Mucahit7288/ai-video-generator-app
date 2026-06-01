83f4110bc43159517e03e6b35714a2bcf6ea2eae
Mücahit Aydın <mucahit7288@gmail.com>
2026-06-01 19:20:51 +0300

    Koddaki APİ key Temizlendi.

--- Flutter/lib/services/gemini_service.dart
+++ Flutter/lib/services/gemini_service.dart
@@ -1,7 +1,7 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 
 class GeminiService {
-  final String _apiKey = "AIzaSyBt8OGgQ4O1zjHb_EwXqDG92BllZUqE0nM";
+  String _apiKey = "REPLACE_WITH_YOUR_API_KEY";
 
   static const String _systemInstruction =
       "Sen yaratıcı bir hikaye yazarısın. "
